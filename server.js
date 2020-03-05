7228bbab7b22f747bba48236581f4fd7aa60b346
sammykhaleel <sammykhaleel83@gmail.com>
2020-03-04 20:18:13 -0500

    pushing

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-// server.js
-var express = require("express");
-var path = require("path");
-var serveStatic = require("serve-static");
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 8080;
+// // server.js
+// var express = require("express");
+// var path = require("path");
+// var serveStatic = require("serve-static");
+// app = express();
+// app.use(serveStatic(__dirname + "/dist"));
+// var port = process.env.PORT || 8080;
+// app.listen(port);
+// console.log("server started " + port);
+
+const express = require("express");
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+  res.sendfile(__dirname + "/dist/index.html");
+});
 app.listen(port);
-console.log("server started " + port);
+
+console.log("Server started...");
